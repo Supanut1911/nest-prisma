843d632948e76581db3dd246efe572206572a36b
Supanut1911 <ynotmela@gmail.com>
2023-01-26 16:35:57 +0700

    api-post

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
